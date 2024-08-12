3b52c73ed2d3cec6417f1e62f0bcecd4a169d11a
GaneshPatil1704 <pyogita868@gmail.com>
2024-08-13 01:28:40 +0530

    react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+
+function App() {
+    return ( <
+        div className = "" >
+        <
+        Navigation / >
+        <
+        div > Hello Ganesh < /div>
+
+        <
+        /div>
+    );
+}
+
+export default App;
